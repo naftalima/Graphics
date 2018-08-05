10d22940a16cf541b7be44747a0a081d8dcd31d3
Nathalia Paiva Lima <npl@cin.ufpe.br>
2018-08-05 17:25:03 -0300

    Battery Power Chart

---
+++ BatteryPower.r
@@ -0,0 +1,14 @@
+#print(getwd())
+
+data <- read.csv("arquivo.csv")
+x <- data$Time12
+y <- data$BatteryPower
+
+png(file = "BatteryChart.png")
+
+plot(x,y, type= "b",col= "tomato", xlab= "Time", ylab = "Battery Power")
+
+
+dev.off() 
+
+#quit()
