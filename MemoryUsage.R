09b337a8481b255b2727872b06117291aedd0573
Nathalia Paiva Lima <npl@cin.ufpe.br>
2018-08-06 22:21:44 -0300

    Update MemoryUsage.R

--- MemoryUsage.R
+++ MemoryUsage.R
@@ -6,7 +6,7 @@ y <- data$MemoryUsage
 
 png(file = "MemoryUsage.png")
 
-plot(x,y, type= "b",col= "orange1", xlab= "Time", ylab = "Memory Usage")
+plot(x,y, type= "b",col= "orange1", xlab= "Time [ms]", ylab = "Memory Usage [KB]")
 
 
 dev.off() 
