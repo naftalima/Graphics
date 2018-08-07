ecdc7acbac485c3ef5063e30adc48f97cf101aec
Nathalia Paiva Lima <npl@cin.ufpe.br>
2018-08-06 22:21:05 -0300

    Update GPULoad.R

--- GPULoad.R
+++ GPULoad.R
@@ -6,7 +6,7 @@ y <- data$GPULoad
 
 png(file = "GPULoad.png")
 
-plot(x,y, type= "b",col= "forestgreen", xlab= "Time", ylab = "GPU Load")
+plot(x,y, type= "b",col= "forestgreen", xlab= "Time [ms]", ylab = "GPU Load [%]")
 
 
 dev.off() 
