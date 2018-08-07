2798f4026fc40793b12a2f1b7ef71351c5f6a461
Nathalia Paiva Lima <npl@cin.ufpe.br>
2018-08-06 22:18:09 -0300

    Update CPU_Load_Normalized.r

--- CPU_Load_Normalized.r
+++ CPU_Load_Normalized.r
@@ -6,7 +6,7 @@ y <- data$CPULoadNormalized
 
 png(file = "CPULoadNormalized.png")
 
-plot(x,y, type= "b",col= "steelblue4", xlab= "Time", ylab = "CPU Load (Normalized)")
+plot(x,y, type= "b",col= "steelblue4", xlab= "Time  [ms]", ylab = "CPU Load (Normalized) [%]")
 
 
 dev.off() 
